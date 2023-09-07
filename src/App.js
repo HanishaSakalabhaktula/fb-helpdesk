04d4a8004ca6f4ece335b1f11cc893ccf1e2d6cb
Sakalabhaktula Hanisha <87903473+Sakalabhaktula-Hanisha@users.noreply.github.com>
2023-09-07 13:59:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Connect from './pages/Connect'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Delete from './pages/Delete'
+import Agent from './pages/Agent'
+import Home from './pages/Home'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='/login' element={<Login />}/>
+    <Route path='/register' element={<Register />}/>
+    <Route path='/delete' element={<Delete />}/>
+    <Route path='/connect' element={<Connect />}/>
+    <Route path='/agent' element={<Agent />} />
+   </Routes>
+  )
 }
 
-export default App;
+export default App
